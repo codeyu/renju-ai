138c241f52da465fd3005e29918b49cfab2cba51
codeyu <codeyu@gmail.com>
2017-11-15 20:06:36 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # renju-ai
+
+http://www.cnblogs.com/erwin/p/7828956.html
---
+++ consts.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+N = 15
+
+
+class ChessboardState(Enum):
+    EMPTY = 0
+    BLACK = 1
+    WHITE = 2
